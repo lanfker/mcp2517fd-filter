a038de8f46b8aa29dc801e20b2665d777a69f690
Chuan Li <chuan.li@akka-na.com>
2019-07-22 16:29:02 -0400

    bugfix..

--- src/canfd-filters.cc
+++ src/canfd-filters.cc
@@ -43,6 +43,7 @@ string CANFDFilterGenerator::gen_filters (vector<int>& canids, const size_t pair
 			for (auto& id : p.canids) {
 				unique_canids.erase(id);
 			}
+			//cout<<" p.canids " <<p.canids.size()  <<" unique_can.size " << unique_canids.size() <<" budget " << bit_budget<< endl;
 			if ( unique_canids.size()  == 0) break; //
 		}
 		if ( unique_canids.size()  == 0) { //
@@ -58,7 +59,7 @@ void CANFDFilterGenerator::print_all_mask_filter_pairs(vector<KS> &knapsacks) {
 	for (auto k : knapsacks) {
 		cout<<" diff_cnt " <<k.diff_cnt <<" common " << k.common.to_string() << " state " << k.state.to_string() << " ids {";
 		for (auto id : k.canids) {
-			cout<<hex<<id<<", ";
+			cout<<setfill('0') << setw(8)<<hex<<id<<", ";
 		}
 		cout<<"}"<<dec<<endl;
 	}
@@ -110,6 +111,24 @@ KS CANFDFilterGenerator::gen_single_pair (unordered_set<int>& canids, int budget
 				dp[key] = _ks;
 			}
 
+			for (auto& item : dp) {
+			if (item.first == key) continue;
+			if ((item.first | key) == key) {
+				//merge...
+				for (auto id : item.second.canids) {
+					if (dp[key].canids.count(id)) continue;
+					bitset<11> id_to_add{(ull)id};
+					for (int i = 0; i < 11; ++ i) {
+						if (dp[key].state.test(i)) id_to_add.reset(i);
+					}
+					if ( (id_to_add.to_ullong() & dp[key].common.to_ullong()) == dp[key].common.to_ullong() ) {
+						dp[key].canids.insert (id);
+					}
+				}
+			}
+		}
+
+
 			if (diff<=budget && diff >= budget_min && dp[key].canids.size() > candidate_size) {
 				ks = dp[key];
 			}
@@ -122,13 +141,44 @@ KS CANFDFilterGenerator::gen_single_pair (unordered_set<int>& canids, int budget
 
 string CANFDFilterGenerator::gen_bash_file_content (vector<KS> &knapsacks, int ifindex) {
 	ostringstream oss;
+
+	oss<<"#!/bin/sh\n";
+
+	/*
+	echo 0x03020100 > /sys/class/net/can1/c1fltcon0
+	echo 0x07060504 > /sys/class/net/can1/c1fltcon1
+	echo 0x0b0a0908 > /sys/class/net/can1/c1fltcon2
+	echo 0x0f0e0d0c > /sys/class/net/can1/c1fltcon3
+	echo 0x13121110 > /sys/class/net/can1/c1fltcon4
+	echo 0x17161514 > /sys/class/net/can1/c1fltcon5
+	echo 0x1b1a1918 > /sys/class/net/can1/c1fltcon6
+	echo 0x1f1e1d1c > /sys/class/net/can1/c1fltcon7
+	*/
+
+
+	oss<<"echo 0x03020100> /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltcon0\n"
+			<< "echo 0x07060504 > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltcon1\n"
+			<< "echo 0x0b0a0908 > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltcon2\n"
+			<< "echo 0x0f0e0d0c > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltcon3\n"
+			<< "echo 0x13121110 > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltcon4\n"
+			<< "echo 0x17161514 > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltcon5\n"
+			<< "echo 0x1b1a1918 > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltcon6\n"
+			<< "echo 0x1f1e1d1c > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltcon7\n";
+
 	for (size_t i = 0; i < knapsacks.size();++ i) {
-		oss<<"echo  0x400007ff > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"mask"<<i<<"\n";
-		oss<<"echo  0x"<<hex<<(int)knapsacks[i].common.to_ullong() <<dec <<" > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltobj"<<i<<"\n\n";
+		knapsacks[i].state.flip();
+		//oss<<"echo  0x400007ff > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"mask"<<i<<"\n";
+		oss<<"echo  0x"<< setfill('0') << setw(8)<<hex<< ((int)knapsacks[i].state.to_ullong()|0x40000000) <<dec <<" > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"mask"<<i<<"\n";
+		oss<<"echo  0x"<< setfill('0') << setw(8)<<hex<<(int)knapsacks[i].common.to_ullong() <<dec <<" > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltobj"<<i<<"\n\n";
 	}
 	for (int i = knapsacks.size(); i < 32; ++ i) {
 		oss<<"echo  0x400007ff > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"mask"<<i<<"\n";
 		oss<<"echo  0x00000000 > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltobj"<<i<<"\n\n";
 	}
+	oss<<"echo 0x8b8a8988 > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltcon0\n"
+			<< "echo 0x8f8e8d8c > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltcon1\n"
+			<< "echo 0x93929190 > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltcon2\n"
+			<< "echo 0x97969594 > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltcon3\n"
+			<< "echo 0x009a9998 > /sys/class/net/can"<<ifindex<<"/c"<<ifindex<<"fltcon4\n";
 	return oss.str();
 }
